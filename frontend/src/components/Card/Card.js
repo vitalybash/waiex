ea60d1fb33ff86c48f243fa5c954a56dbfaa33ef
Goddo-ro <r0manovic1@yandex.ru>
2023-03-08 22:35:22 +0300

    Complete developers section

--- frontend/src/components/Card/Card.js
+++ frontend/src/components/Card/Card.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import "./Card.css";
 
-const Card = ({developer}) => {
+const Card = ({user}) => {
+  console.log("../../assets/" + user.image);
   return (
-    <div>
-      
+    <div className="card">
+      <img src={require("../../assets/" + user.image)} />
+      <div className="user-info">
+        <div>
+          <p>{user.description}</p>
+          <div className="stack">
+            {user.stack?.map(item => <div className="stack-item">{item}</div>)}
+          </div>
+        </div>
+        <img src={require("../../assets/" + user.logo)} />
+      </div>
     </div>
   );
 };
--- frontend/src/components/DevelopersGrid/DevelopersGrid.js
+++ frontend/src/components/DevelopersGrid/DevelopersGrid.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Card from "../Card/Card";
+import "./DevelopersGrid.css";
 
 const DevelopersGrid = ({data}) => {
   return (
     <div className="grid-container">
-
+      {data.users?.map(user => <Card key={user.id} user={user} />)}
     </div>
   );
 };
