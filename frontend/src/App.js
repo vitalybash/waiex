5981ed3614ddbce75cf086d4d5e4850652f2a4ad
Goddo-ro <r0manovic1@yandex.ru>
2023-03-08 21:22:54 +0300

    Complete header styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import Header from "./components/UI/Header/Header";
+import "./App.css";
 
 function App() {
   return (
--- frontend/src/components/UI/Header/Header.js
+++ frontend/src/components/UI/Header/Header.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import logo from '../../../assets/logo.png';
+import './Header.css';
 
 const Header = () => {
   return (
     <header>
-
+      <div className="left">
+        <img id="logo" src={logo} alt="Logo" />
+        <div className="pages">
+          <a href="#">Проекты</a>
+          <a href="#">Специалисты</a>
+          <a href="#">Кабинет</a>
+        </div>
+      </div>
+      <a href="#">Ахиллес Сын Пелея</a>
     </header>
   );
 };
