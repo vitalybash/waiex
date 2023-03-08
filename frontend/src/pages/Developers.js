f83444325322dcedacd7692b9289e38f8decb9a2
Goddo-ro <r0manovic1@yandex.ru>
2023-03-08 21:28:01 +0300

    Developers page init

---
+++ frontend/src/pages/Developers.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MyInput from "../components/UI/Input/MyInput";
+
+const Developers = () => {
+  return (
+    <section className="developers">
+      <MyInput />
+    </section>
+  );
+};
+
+export default Developers;
