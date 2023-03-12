5a5f1ed439b8ee00ae033e1cf553aa82a5a89a42
Goddo-ro <r0manovic1@yandex.ru>
2023-03-12 18:34:50 +0300

    Add useFetching hook

--- frontend/src/API/ServicesService.js
+++ frontend/src/API/ServicesService.js
@@ -1,20 +1,20 @@
-import services from "../data/services.json";
+import axios from "axios";
 
 export default class ServicesService {
-  static getAll() {
-    return services.services;
+  static async getAll() {
+    return await axios.get('http://127.0.0.1:8000/skills/');
   }
 
-  static getByUserId(id) {
-    const servicesArr = services.services;
-    const result = [];
-    for (let i = 0; i < servicesArr.length; i++) {
-      const service = servicesArr[i];
-      if (service["user_id"] == id) {
-        result.push(service);
-      }
-    }
-
-    return result;
-  }
+  // static getByUserId(id) {
+  //   const servicesArr = services.services;
+  //   const result = [];
+  //   for (let i = 0; i < servicesArr.length; i++) {
+  //     const service = servicesArr[i];
+  //     if (service["user_id"] == id) {
+  //       result.push(service);
+  //     }
+  //   }
+  //
+  //   return result;
+  // }
 }
