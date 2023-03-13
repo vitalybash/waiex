a7c8242276195e8f25ad17fd787880230031d415
SKAT1005 <111747034+SKAT1005@users.noreply.github.com>
2023-03-13 18:49:30 +0300

    Update models.py

--- backend/waiex/executors/models.py
+++ backend/waiex/executors/models.py
@@ -11,12 +11,8 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     avatar = models.ImageField(upload_to='avatar', verbose_name='аватар пользователя')
     name = models.CharField(max_length=256, default='', verbose_name='Имя исполнителя')
     surname = models.CharField(max_length=256, default='', verbose_name='Фамилия исполнителя')
-    skills = models.ForeignKey('Skill', blank=True, null=True ,on_delete=models.CASCADE, verbose_name='карточка',
-                               related_name='skill')
     age = models.IntegerField(default=0, verbose_name='Возраст исполнителя')
     info = models.CharField(max_length=1024, default='', verbose_name='Общая информация о исполнителе')
-    reviews = models.ForeignKey('Reviews', on_delete=models.CASCADE, null=True, related_name='review',
-                                verbose_name='Отзывы исполнителя', blank=True)
     reviews_count = models.IntegerField(default=0, verbose_name='Кол-во отзывов')
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
@@ -34,17 +30,20 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
 
 class Reviews(models.Model):  # Модель отзывов
-    """author = models.OneToOneField('Заказчик', on_delete=models.CASCADE, related_name='authors',
-                                  verbose_name='Автор отзыва')"""
+    user_from = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='review_autor',
+                                  verbose_name='Автор отзыва')
+    user_to = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='user_to',
+                                   verbose_name='Адресат отзыва')
     text = models.CharField(max_length=5096, verbose_name='Текст отзыва')
     estimation = models.IntegerField(default=0, verbose_name='Оценка')
 
 
 class Skill(models.Model):  # Модель услуг
+    autor = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='card_autor',
+                              verbose_name='Автор карточки')
     image = models.ImageField(upload_to='image', verbose_name='фотография услуги')
     title = models.CharField(max_length=256, verbose_name='Заголовок услуги')
     description = models.TextField(max_length=1024, verbose_name='Описание услуги')
     stack = models.CharField(max_length=512, verbose_name='Стек технологии')
     price = models.IntegerField(default=0, verbose_name='Цена услуги')
-
 
