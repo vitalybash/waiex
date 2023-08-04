c4b2287a68cd1109d773027f47d19bb8ab64fff1
Goddo-ro <r0manovic1@yandex.ru>
2023-08-04 22:09:24 +0300

    Add error message on login, registration and skills page; Change styles of login and register modal windows

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -4,22 +4,32 @@ import "../styles/form.css";
 import { useDispatch, useSelector } from "react-redux";
 import { login } from "../actions/auth";
 import { clearMessage } from "../actions/message";
+import { CLEAR_MESSAGE, SET_MESSAGE } from "../actions/types";
+import Loader from "./UI/Loader/Loader";
 
 const LoginForm = ({setVisible, setRegister}) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+
+  const {message} = useSelector(state => state.message);
 
   const dispatch = useDispatch();
 
   const onChangeEmail = (e) => {
     const email = e.target.value;
     setEmail(email);
+    dispatch({
+      type: CLEAR_MESSAGE,
+    });
   };
 
   const onChangePassword = (e) => {
     const password = e.target.value;
     setPassword(password);
+    dispatch({
+      type: CLEAR_MESSAGE,
+    });
   };
 
   const clearFields = () => {
@@ -46,25 +56,34 @@ const LoginForm = ({setVisible, setRegister}) => {
 
   return (
     <form className="modal-form">
-      <h3>Вход</h3>
-      <MyInput placeholder="Логин" id="login"
-               value={email} onChange={onChangeEmail} />
-      <MyInput type="password" placeholder="Пароль" id="password"
-               value={password} onChange={onChangePassword} />
-      <div>
-        <a className="shadow my-input">Забыли пароль</a>
-        <a className="shadow my-input" onClick={(e) => handleLogin(e)}>Войти</a>
-      </div>
-      <p>Еще нет учетной записи?
-        <a onClick={() => {
-            clearFields();
-            setVisible(false);
-            setRegister(true);
-          }
-        }>
-          Регистрация
-        </a>
-      </p>
+      {
+        isLoading ? <Loader/>
+
+
+          :
+          <>
+            <h3>Вход</h3>
+            <MyInput placeholder="Логин" id="login"
+                     value={email} onChange={onChangeEmail} />
+            <MyInput type="password" placeholder="Пароль" id="password"
+                     value={password} onChange={onChangePassword} />
+            <div>
+              <a className="shadow my-input">Забыли пароль</a>
+              <a className="shadow my-input" onClick={(e) => handleLogin(e)}>Войти</a>
+            </div>
+            {message && <h3 className="error-text">Error: {message}</h3>}
+            <p>Еще нет учетной записи?
+              <a onClick={() => {
+                clearFields();
+                setVisible(false);
+                setRegister(true);
+              }
+              }>
+                Регистрация
+              </a>
+            </p>
+          </>
+      }
     </form>
   );
 };
--- frontend/src/components/RegisterForm.js
+++ frontend/src/components/RegisterForm.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import MyInput from "./UI/Input/MyInput";
 import "../styles/form.css";
-import Error from "./UI/Error/Error";
 import { useDispatch, useSelector } from "react-redux";
 import { register } from "../actions/auth";
 import { clearMessage } from "../actions/message";
+import Loader from "./UI/Loader/Loader";
 
 const RegisterForm = ({setVisible, setLogin}) => {
   const [email, setEmail] = useState("");
@@ -15,6 +15,7 @@ const RegisterForm = ({setVisible, setLogin}) => {
   const [passwordError, setPasswordError] = useState("");
   const [error, setError] = useState("");
   const [successful, setSuccessful] = useState(false);
+  const [isLoading, setLoading] = useState(false);
 
   const { message } = useSelector(state => state.message);
   const dispatch = useDispatch();
@@ -37,7 +38,7 @@ const RegisterForm = ({setVisible, setLogin}) => {
     e.preventDefault();
 
     setSuccessful(false);
-
+    setLoading(true);
 
     dispatch(register(username, email, password))
       .then(() => {
@@ -45,6 +46,7 @@ const RegisterForm = ({setVisible, setLogin}) => {
       })
       .catch(() => {
         setSuccessful(false);
+        setLoading(false);
       });
   };
 
@@ -62,41 +64,43 @@ const RegisterForm = ({setVisible, setLogin}) => {
 
   return (
     <form className="modal-form">
-      <h3>Регистрация</h3>
-      <MyInput placeholder="Почта" id="emailR"
-               value={email} onChange={handleChange}
-      />
-      { emailError && <Error message={emailError} /> }
-      <MyInput placeholder="Логин" id="loginR"
-               value={username} onChange={(e) => setUsername(e.target.value)}
-      />
-      <MyInput type="password" placeholder="Пароль" id="passwordR"
-               value={password} onChange={(e) => setPassword(e.target.value)}
-      />
-      <MyInput type="password" placeholder="Повторите пароль" id="repeat_password"
-               value={repeatPassword} onChange={(e) => setRepPassword(e.target.value)}
-      />
-      { passwordError && <Error message={passwordError} /> }
-      { error && <Error message={error} /> }
-      <div>
-        <a className="shadow my-input" onClick={(e) => handleRegister(e)}>Регистрация</a>
-      </div>
-      <p>Уже есть аккаунт?
-        <a onClick={() => {
-          clearFields();
-          setVisible(false);
-          setLogin(true);
-        }}>
-          Войти
-        </a>
-      </p>
-      {message && (
-        <div className="form-group">
-          <div className={ successful ? "alert alert-success" : "alert alert-danger" } role="alert">
-            {message}
-          </div>
-        </div>
-      )}
+      {
+        isLoading ? <Loader/>
+          : <>
+            <h3>Регистрация</h3>
+            <MyInput placeholder="Почта" id="emailR"
+                     value={email} onChange={handleChange}
+            />
+            { emailError && <h3 className="error-text">{emailError}</h3> }
+            <MyInput placeholder="Логин" id="loginR"
+                     value={username} onChange={(e) => setUsername(e.target.value)}
+            />
+            <MyInput type="password" placeholder="Пароль" id="passwordR"
+                     value={password} onChange={(e) => setPassword(e.target.value)}
+            />
+            <MyInput type="password" placeholder="Повторите пароль" id="repeat_password"
+                     value={repeatPassword} onChange={(e) => setRepPassword(e.target.value)}
+            />
+            { passwordError && <h3 className="error-text">{passwordError}</h3> }
+            { error && <h3 className="error-text">{error}</h3>}
+
+            <div>
+              <a className="shadow my-input" onClick={(e) => handleRegister(e)}>Регистрация</a>
+            </div>
+
+            {message && <h3 className="error-text">{message}</h3>}
+
+            <p>Уже есть аккаунт?
+              <a onClick={() => {
+                clearFields();
+                setVisible(false);
+                setLogin(true);
+              }}>
+                Войти
+              </a>
+            </p>
+          </>
+      }
     </form>
   );
 };
